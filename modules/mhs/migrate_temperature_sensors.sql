5c6ef4f27415a306419578c6822b47537589f5e1
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-06-12 11:51:38 +0200

    fix(modules/mhs): improve migrate serial number of temperature sensors

--- modules/mhs/migrate_temperature_sensors.sql
+++ modules/mhs/migrate_temperature_sensors.sql
@@ -10,17 +10,21 @@ BEGIN;
 INSERT INTO pr_monitoring_habitat_station.t_temperature_sensors (id_transect, serial_number, install_date)
     SELECT
         t.id_transect,
-        nullif(regexp_replace(
-            (regexp_matches(bs.base_site_description,
+        coalesce(
+            regexp_substr(
+                bs.base_site_description,
                 'Capteur(?:\s+de)?\s+temp[eé]rature\s+n°([^\s(]+)\s*\(',
-                'i'))[1],
-            '[^0-9]', '', 'g'
-        ), '') AS serial_number,
+                1, 1, 'i', 1
+            ),
+            '..?..'
+        ) AS serial_number,
         to_date(
             regexp_replace(
-                (regexp_matches(bs.base_site_description,
+                regexp_substr(
+                    bs.base_site_description,
                     '\((\d{4}[-/]\d{2}[-/]\d{2})\)',
-                    'i'))[1],
+                    1, 1, 'i', 1
+                ),
                 '/', '-', 'g'
             ),
             'YYYY-MM-DD'
@@ -28,9 +32,8 @@ INSERT INTO pr_monitoring_habitat_station.t_temperature_sensors (id_transect, se
     FROM gn_monitoring.t_base_sites AS bs
         JOIN pr_monitoring_habitat_station.t_transects AS t
             ON t.id_base_site = bs.id_base_site
-    WHERE lower(bs.base_site_description) LIKE '%capteur%'
-        AND lower(bs.base_site_description) LIKE '%n°%'
-        AND bs.base_site_description ~ '\(\d{4}[-/]\d{2}[-/]\d{2}\)' ;
+    WHERE bs.base_site_description 
+        ~ 'Capteur(?:\s+de)?\s+temp[eé]rature\s+n°[^(]+\(\d{4}[-/]\d{2}[-/]\d{2}\)' ;
 
 
 \echo '----------------------------------------------------------------------------'
